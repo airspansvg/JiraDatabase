d52a7d12c4e5d08b3d05f00cbfab4e7fa4e84076
srhudda <shudda@airspan.com>
2024-09-28 22:20:58 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name="JiraDatabase",
-    version="0.1",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[],
