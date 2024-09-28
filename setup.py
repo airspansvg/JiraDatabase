0173292d9090765361cd83799ca8f07589030c75
srhudda <shudda@airspan.com>
2024-09-28 22:20:48 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     include_package_data=True,
     install_requires=[],
     package_data={
-        "": ['*.json'],  # Ensure the JSON files are included in the package
+        "": ["*.json"],  # Ensure the JSON files are included in the package
     },
     description="JIRA data backup package",
     author="Sitaram Hudda",
