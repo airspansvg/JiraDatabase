f378e162f1caba67a58fdbb8ccc09018a558887e
srhudda <shudda@airspan.com>
2024-10-05 19:03:09 +0530

    Update find_values.py

--- JiraDatabase/find_values.py
+++ JiraDatabase/find_values.py
@@ -1,18 +1,28 @@
 import json
 import os
 
-type = "subtask"
-param = "Product support"
 
-package_dir = os.path.dirname(__file__)
-directory = package_dir + "\\" + type + "\\"
-param_list = []
-for filename in os.listdir(directory):
-    if filename.endswith('.json'):
-        with open(directory + filename, 'r') as fp:
-            json_dict = json.load(fp)
-            if json_dict.get(param):
-                param_list.append(json_dict.get(param))
-final_list = list(set(param_list))
-list_of_tuple = [(item, item) for item in final_list]
-print(list_of_tuple)
+def get_all_params_and_possible_values(type):
+    base_dir = os.path.dirname(__file__)
+    directory = base_dir + "\\" + type + "\\"
+    data_dict = {}
+    for filename in os.listdir(directory):
+        print(f"checking : {filename}")
+        if filename.endswith('.json'):
+            with open(directory + filename, 'r') as fp:
+                json_dict = json.load(fp)
+                for key, val in json_dict.items():
+                    data_dict.setdefault(key, [])
+                    if isinstance(val, list):
+                        for elem in val:
+                            if elem not in data_dict[key]:
+                                data_dict[key].append(elem)
+                    else:
+                        if not val in data_dict[key]:
+                            data_dict[key].append(val)
+#        print(data_dict)
+    return data_dict
+
+if __name__ == "__main__":
+    type = "testplan"
+    print(get_all_params_and_possible_values(type))
